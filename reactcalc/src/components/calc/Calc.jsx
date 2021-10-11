f4cd60a0e31f42e677e49997127bbe5773e99c64
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-10-11 16:39:45 -0400

    add operator

--- reactcalc/src/components/calc/Calc.jsx
+++ reactcalc/src/components/calc/Calc.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react"
+import './Calc.css'
 
 const Calc = () => {
 
+    const [operator, setOperator] = useState('+')
     const [num1, setNum1] = useState(0)
     const [num2, setNum2] = useState(0)
     const [total, setTotal] = useState(num1 + num2)
 
-    function sum(){
-        setTotal(num1 + num2)
+    function calculator(){
+        if(operator === '+') setTotal(num1 + num2)
+        if(operator === '-') setTotal(num1 - num2)
+        if(operator === '*') setTotal(num1 * num2)
+        if(operator === '/') setTotal(num1 / num2)
     }
 
     return (
         <div className="calc">
             <input type="number" value={num1} onChange={(e) => setNum1(Number(e.target.value))} />
+            <input type="text" value={operator} onChange={(e) => setOperator(e.target.value)} />
             <input type="number" value={num2} onChange={(e) => setNum2(Number(e.target.value))} />
-            <button onClick={sum}>calcular</button>
+            <button onClick={calculator}>calcular</button>
             <p>{total}</p>
         </div>
     )
